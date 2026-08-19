8e2b3cdbf15a02b42e72a3683087b3604e5777d9
CM-ONE <one.cmeg@gmail.com>
2026-08-19 21:34:45 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -7,12 +7,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyCXyKSTlmlzkYnH2LW408cVVWV1CPvlfBo",
-    authDomain: "cmfilings-6a37c.firebaseapp.com",
-    projectId: "cmfilings-6a37c",
-    storageBucket: "cmfilings-6a37c.firebasestorage.app",
-    messagingSenderId: "138705123778",
-    appId: "1:138705123778:web:9561bff9f0f5d89bb6fe5b"
+      apiKey: "AIzaSyCXyKSTlmlzkYnH2LW408cVVWV1CPvlfBo",
+  authDomain: "cmfilings-6a37c.firebaseapp.com",
+  projectId: "cmfilings-6a37c",
+  storageBucket: "cmfilings-6a37c.firebasestorage.app",
+  messagingSenderId: "138705123778",
+  appId: "1:138705123778:web:9561bff9f0f5d89bb6fe5b",
 };
 
 export const app = initializeApp(firebaseConfig);
